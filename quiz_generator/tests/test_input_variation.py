54257c00f5f8949010f4b520b877451439f9c5f5
Janis Lesinskis <footbagfreestyle@gmail.com>
2017-10-03 06:50:45 +0000

    Test user supplied input selection function

--- quiz_generator/tests/test_input_variation.py
+++ quiz_generator/tests/test_input_variation.py
@@ -11,3 +11,18 @@ def test_sequential_variation_storage():
     variation = Variation(items)
     generated = variation.get()
     assert all(a == b for a, b in itertools.zip_longest(generated, items))
+
+def test_reversed_selection():
+    """Test that variation is able to be stored in a sequentially accessible manner"""
+
+    def reversed_selection(items):
+        """Yield items from an iterable in reverse order"""
+        for item in reversed(items):
+            yield item
+
+    items = ["a", "b", "c"]
+    variation = Variation(items, selection_method=reversed_selection)
+    generated = variation.get()
+    expected = ["c", "b", "a"]
+    assert all(a == b for a, b in itertools.zip_longest(generated, expected))
+
